8bedd382e483b1de0f6fb00a74cbc8eacbee5f90
vishal0972 <30875612+vishal0972@users.noreply.github.com>
2017-08-15 18:25:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/ui/namaste.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+  res.sendFile(path.join(__dirname, 'ui', 'namaste.png'));
 });
 
 
